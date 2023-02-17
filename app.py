841c75ebc885cf54595e7d4b63a2aecbc1041fe3
Watson Chua <watsonchua@gmail.com>
2023-02-17 18:42:56 +0800

    add transcript segment

--- app.py
+++ app.py
@@ -42,23 +42,37 @@ def main():
         if 'All' in transcription_filter:
             transcription_filter = all_transcriptions
     
-    synopsis = df[(df["file_id"] == file_select) & (df['transcription_method'] == 'Synopsis')].iloc[0]['content']
+    df_synopsis = df[(df["file_id"] == file_select) & (df['transcription_method'] == 'Synopsis')]
+    # TODO: why are synopsis not showing?
+    synopsis = df_synopsis.iloc[0]['summary'] if len(df_synopsis) > 0 else None
     df_filtered = df[(df["file_id"] == file_select) & (df['transcription_method'].isin(transcription_filter)) & (df['summary_method'].isin(summarizer_filter))]
     
     with st.container():
+        st.subheader('Transcripts')
+        for tm in df_filtered['transcription_method'].unique():
+            df_tm_filtered = df_filtered[df['transcription_method']==tm]
+            with st.expander(tm):
+                st.text_area(label='', value=df_tm_filtered['transcript'].iloc[0], height=500)
+
+
+
         # st.text_area("Synopsis", value=synopsis)
+
+
+    
+    with st.container():
+        st.subheader('Summaries')
+
         with st.expander("Synopsis", expanded=True):
             st.write(fr"{synopsis}")
 
-    
-    
-    for tm in df_filtered['transcription_method'].unique():
-        with st.expander(tm):
-            df_tm_filtered = df_filtered[df['transcription_method']==tm]
-            for index, row in df_tm_filtered.iterrows():
-                # st.text_area(label=row['summary_method'], value=row['content'])
-                st.caption(fr"{row['summary_method']}")
-                st.write(fr"{row['content']}")
+        for tm in df_filtered['transcription_method'].unique():
+            with st.expander(tm):
+                df_tm_filtered = df_filtered[df['transcription_method']==tm]
+                for index, row in df_tm_filtered.iterrows():
+                    # st.text_area(label=row['summary_method'], value=row['content'])
+                    st.caption(fr"{row['summary_method']}")
+                    st.write(fr"{row['summary']}")
 
 
 
